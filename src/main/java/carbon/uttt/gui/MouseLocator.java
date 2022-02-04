8c9b1d31bf58325feec2d19a0c136125f6588ebd
Carbon225 <carbon225@protonmail.com>
2022-02-04 14:48:23 +0100

    mouse interaction

---
+++ src/main/java/carbon/uttt/gui/MouseLocator.java
@@ -0,0 +1,34 @@
+package carbon.uttt.gui;
+
+import carbon.uttt.game.Pos3x3;
+import carbon.uttt.game.PosDPMP;
+import javafx.beans.property.DoubleProperty;
+
+public class MouseLocator {
+
+    private final DoubleProperty width, height;
+
+    public MouseLocator(DoubleProperty width, DoubleProperty height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public PosDPMP locateMouse(double x, double y) {
+        if (x < 0 || x >= width.doubleValue() || y < 0 || y >= height.doubleValue())
+            return null;
+
+        x *= 3. / width.doubleValue();
+        y *= 3. / height.doubleValue();
+
+        int dpx = (int) x;
+        int dpy = (int) y;
+
+        int mpx = (int) (((x - dpx - 0.05) / 0.9 - 0.05) / 0.9 * 3.);
+        int mpy = (int) (((y - dpy - 0.05) / 0.9 - 0.05) / 0.9 * 3.);
+
+        if (mpx < 0 || mpx > 2 || mpy < 0 || mpy > 2)
+            return null;
+
+        return new PosDPMP(Pos3x3.fromXY(dpx, dpy), Pos3x3.fromXY(mpx, mpy));
+    }
+}
--- src/main/java/carbon/uttt/gui/controller/GameController.java
+++ src/main/java/carbon/uttt/gui/controller/GameController.java
@@ -1,18 +1,20 @@
 package carbon.uttt.gui.controller;
 
 import carbon.uttt.game.Game;
-import carbon.uttt.game.Pos3x3;
 import carbon.uttt.game.PosDPMP;
 import carbon.uttt.gui.IDrawable;
 import carbon.uttt.gui.IDrawableObserver;
+import carbon.uttt.gui.MouseLocator;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
 
 public class GameController implements IDrawableObserver {
 
     private final Game game = new Game();
+    private MouseLocator mouseLocator;
 
     @FXML
     public Canvas canvas;
@@ -23,14 +25,33 @@ public class GameController implements IDrawableObserver {
 
     @FXML
     public void initialize() {
+        mouseLocator = new MouseLocator(
+                canvas.widthProperty(),
+                canvas.heightProperty()
+        );
+
         game.newGame();
-        game.makeMove(new PosDPMP(Pos3x3.C, Pos3x3.NE));
-        game.makeMove(new PosDPMP(Pos3x3.NE, Pos3x3.NE));
-        game.makeMove(new PosDPMP(Pos3x3.NE, Pos3x3.C));
-        game.makeMove(new PosDPMP(Pos3x3.C, Pos3x3.N));
+
+        canvas.setOnMouseClicked(this::onMouseClicked);
+        canvas.setOnMouseMoved(this::onMouseMoved);
+        canvas.setOnMouseExited(this::onMouseExited);
     }
 
-    public void updateUI() {
+    private void onMouseClicked(MouseEvent e) {
+        PosDPMP dpmp = mouseLocator.locateMouse(e.getX(), e.getY());
+        game.makeMove(dpmp);
+    }
+
+    private void onMouseMoved(MouseEvent e) {
+        PosDPMP dpmp = mouseLocator.locateMouse(e.getX(), e.getY());
+        game.highlightMove(dpmp);
+    }
+
+    private void onMouseExited(MouseEvent e) {
+        game.highlightMove(null);
+    }
+
+    private void updateUI() {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         gc.save();
         gc.scale(canvas.getWidth(), canvas.getHeight());
